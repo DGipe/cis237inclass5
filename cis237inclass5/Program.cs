00e0950566ce6df46079b995f557200afad7ba28
David Gipe <dwgipe@gmail.com>
2017-03-22 19:35:32 -0400

    database connectino

--- cis237inclass5/Program.cs
+++ cis237inclass5/Program.cs
@@ -10,6 +10,20 @@ namespace cis237inclass5
     {
         static void Main(string[] args)
         {
+            //Make new instance of the Cars collection
+            //propbly goint to be CarsInitial Name for you 
+            CarsDGipeEntities carsDGipeEntities = new CarsDGipeEntities();
+
+            //***************************
+            //List out all of the cars in the table
+            //*************************
+
+            Console.WriteLine("Print the Lis");
+
+            foreach(Car car in carsDGipeEntities.Cars)
+            {
+                Console.WriteLine(car.id + " " + car.make + " " + car.model);
+            }
         }
     }
 }
